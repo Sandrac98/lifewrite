d87d905d3a719585877bfa6e7d9d78d67b6ebab2
Sandra C <114778027+Sandrac98@users.noreply.github.com>
2023-06-30 23:47:15 +0000

    Display the journal entry using materialize

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from bson import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 
 if os.path.exists("env.py"):
@@ -62,11 +62,13 @@ def register():
 def login():
     if request.method == "POST":
         # Check if username exists in the database
-        existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})  # noqa
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
 
         if existing_user:
             # Ensure hashed password matches user input
-            if check_password_hash(existing_user["password"], request.form.get("password")):  # noqa
+            if check_password_hash(
+                 existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
                 return redirect(url_for("profile", username=session["user"]))
@@ -78,30 +80,24 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/profile", methods=["GET", "POST"])
-def profile():
-    # Retrieve the user's username from the session
-    username = session.get("username")
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # Retrieve the user's information from the database based
+    #  on the provided username
+    user = mongo.db.users.find_one({"username": username})
 
-    if not username:
-        flash("Please log in to access your profile")
+    if not user:
+        flash("User not found")
         return redirect(url_for("login"))
 
-    # Retrieve the user's information from the database based on the session username  # noqa
-    username = mongo.db.users.find_one({"username": username})
-
-    if not username:
-        flash("User not found :(")
-        return redirect(url_for("login"))
-
-    return render_template("profile.html", username=username["username"])
+    return render_template("profile.html", username=user["username"])
 
 
 @app.route("/logout")
 def logout():
     # remove user from session cookie
     flash("You have been logged out. See you soon!")
-    session.pop("user")
+    session.pop("user", None)
     return redirect(url_for("login"))
 
 
