e33fce7a34306d6b954b9f71441b83e18374087c
Sandra C <114778027+Sandrac98@users.noreply.github.com>
2023-07-01 00:00:16 +0000

    Add new journal function and template

--- app.py
+++ app.py
@@ -101,6 +101,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/new_journal")
+def new_journal():
+    return render_template("new_journal.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
